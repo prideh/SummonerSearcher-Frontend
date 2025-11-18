f85664e5c222341dfae07229ae97f547411f7fe5
Mahdi Nasserzadeh <nasserzadeh@hotmail.ch>
2025-11-18 13:26:17 +0100

    cors fix

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useAuthStore } from '../store/authStore';
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
 });
 
 // Use an interceptor to automatically add the token to every authenticated request
