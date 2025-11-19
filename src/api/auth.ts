63f39a5d83d097e932478c42e170605064b112a5
Mahdi Nasserzadeh <nasserzadeh@hotmail.ch>
2025-11-19 17:30:28 +0100

    fixing 2faw

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -5,7 +5,8 @@ const API_BASE_URL = `${import.meta.env.VITE_API_BASE_URL}/api`;
 interface LoginResponse {
   jwt: string;
   darkmodePreference: boolean;
-  twoFactorEnabled: boolean;
+  twoFactorEnabled: boolean; 
+  twoFactorRequired: boolean;
   tempToken: string;
   recentSearches: string[];
 }
